deb220ea7cd898e62306cd9f55574a5e0ceb1d85
Noah Denefe <ndenefe@smu.edu>
2018-03-02 21:59:10 -0600

    Updated mock-api and handled reloading on change

--- src/app/domain/mock-api.service.ts
+++ src/app/domain/mock-api.service.ts
@@ -21,51 +21,41 @@ export class MockApi implements InMemoryDbService {
             {
                 semester: 'F17',
                 courses: [
-                    {id: 'CHEM 1303', name: 'General Chemistry', type: 'Lecture'},
-                    {id: 'CHEM 1113', name: 'General Chemistry Laboratory', type: 'Lab'},
-                    {id: 'CSE 3342', name: 'Programming Languages', type: 'Lecture'},
-                    {id: 'CSE 3345', name: 'Graphical User Interface', type: 'Lecture'},
+                    {id: 'CHEM 1303-001', name: 'General Chemistry', type: 'Lecture'},
+                    {id: 'CHEM 1113-L07', name: 'General Chemistry Laboratory', type: 'Lab'},
+                    {id: 'CSE 3342-002', name: 'Programming Languages', type: 'Lecture'},
+                    {id: 'CSE 3345-801', name: 'Graphical User Interface', type: 'Lecture'},
                     {id: 'CSE 3381-801', name: 'Digital Logic Design', type: 'Lecture'},
-                    {id: 'CSE 4340', name: 'Beginning Japanese: 2nd Term', type: 'Lecture'},
+                    {id: 'CSE 4340-001C', name: 'Stat Methods for Engineers', type: 'Lecture'},
                 ]
             },
             {
                 semester: 'S17',
                 courses: [
-                    {id: 'CSE 3330-801', name: 'Database Concepts', type: 'Lecture'},
-                    {id: 'CSE 3330-N12C', name: 'Database Concepts', type: 'Lab'},
-                    {id: 'CSE 3339-801', name: 'Information Assurance & Security', type: 'Lecture'},
-                    {id: 'CSE 3339-N13', name: 'Information Assurance & Security', type: 'Lab'},
-                    {id: 'CSE 3353-001', name: 'Fundamentals of Algorythms', type: 'Lecture'},
-                    {id: 'JAPN 1402-801', name: 'Beginning Japanese: 2nd Term', type: 'Lecture'},
-                    {id: 'JAPN 1402-N10', name: 'Beginning Japanese: 2nd Term', type: 'Lab'},
-                    {id: 'ANTH 3319-001', name: 'Humanity & Global Env Change', type: 'Lecture'}
+                    {id: 'CSE 2341-801', name: 'Data Structures', type: 'Lecture'},
+                    {id: 'CSE 2353-001', name: 'Discrete Comp Structures', type: 'Lecture'},
+                    {id: 'CSE 3365-801C', name: 'Intro to Scientific Computing', type: 'Lecture'},
+                    {id: 'PHYS 3305-001', name: 'Introduction to Modern Physics', type: 'Lecture'},
+                    {id: 'PRW 2144-L01', name: 'PRW2: Scuba', type: 'Lab'}
                 ]
             },
             {
                 semester: 'F16',
                 courses: [
-                    {id: 'CSE 3330-801', name: 'Database Concepts', type: 'Lecture'},
-                    {id: 'CSE 3330-N12C', name: 'Database Concepts', type: 'Lab'},
-                    {id: 'CSE 3339-801', name: 'Information Assurance & Security', type: 'Lecture'},
-                    {id: 'CSE 3339-N13', name: 'Information Assurance & Security', type: 'Lab'},
-                    {id: 'CSE 3353-001', name: 'Fundamentals of Algorythms', type: 'Lecture'},
-                    {id: 'JAPN 1402-801', name: 'Beginning Japanese: 2nd Term', type: 'Lecture'},
-                    {id: 'JAPN 1402-N10', name: 'Beginning Japanese: 2nd Term', type: 'Lab'},
-                    {id: 'ANTH 3319-001', name: 'Humanity & Global Env Change', type: 'Lecture'}
+                    {id: 'CSE 2341-801', name: 'Data Structures', type: 'Lecture'},
+                    {id: 'JAPN 1401-801', name: 'Beginning Japanese', type: 'Lecture'},
+                    {id: 'CSE 2240-801', name: 'Assy Lang Prog & Mach Org', type: 'Lecture'},
+                    {id: 'PHYS 1304-001', name: 'Intro Electricity & Magnetism', type: 'Lecture'}
                 ]
             },
             {
                 semester: 'S16',
                 courses: [
-                    {id: 'CSE 3330-801', name: 'Database Concepts', type: 'Lecture'},
-                    {id: 'CSE 3330-N12C', name: 'Database Concepts', type: 'Lab'},
-                    {id: 'CSE 3339-801', name: 'Information Assurance & Security', type: 'Lecture'},
-                    {id: 'CSE 3339-N13', name: 'Information Assurance & Security', type: 'Lab'},
-                    {id: 'CSE 3353-001', name: 'Fundamentals of Algorythms', type: 'Lecture'},
-                    {id: 'JAPN 1402-801', name: 'Beginning Japanese: 2nd Term', type: 'Lecture'},
-                    {id: 'JAPN 1402-N10', name: 'Beginning Japanese: 2nd Term', type: 'Lab'},
-                    {id: 'ANTH 3319-001', name: 'Humanity & Global Env Change', type: 'Lecture'}
+                    {id: 'CSE 1342-801', name: 'Programming Concepts', type: 'Lecture'},
+                    {id: 'EMIS 1305-803', name: 'Computing Technology', type: 'Lecture'},
+                    {id: 'PHYS 1304-002', name: 'Intro Electricity & Magnetism', type: 'Lecture'},
+                    {id: 'PHYS 1106-L14', name: 'Electricity & Magnetism Lab', type: 'Lab'},
+                    {id: 'MATH 3353-002', name: 'Introduction to Linear Algebra', type: 'lecture'}
                 ]
             },
             {
--- src/app/semesters/semesters.component.ts
+++ src/app/semesters/semesters.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Course, CourseList, SemesterRepository } from '../domain';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-semesters',
   templateUrl: './semesters.component.html',
@@ -8,25 +9,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SemestersComponent implements OnInit {
   private courses: Course[];
-  private loaded = false;
+  private loaded;
   private semester: CourseList;
   private s;
   private sub;
   constructor(
     private semesterRepository: SemesterRepository,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef
+  ) {
+    this.loaded = false;
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.s = params['semester'];
-      this.initializeState(); // reset and set based on new parameter this time
-    });
-    this.semesterRepository.getBySemester(this.s)
+      this.cdRef.detectChanges();
+      this.semesterRepository.getBySemester(this.s)
       .subscribe(x => this.onCoursesLoaded(x));
-  }
-  private initializeState() {
-      // location.reload();
+    });
   }
   private onCoursesLoaded(courses: CourseList) {
     this.semester = courses[0];
